50ab96810ef2249f8755ccc1a8ee40dff6ef7587
Hexagon Digital Services <hexagonsservices@gmail.com>
2025-06-22 03:09:15 +0530

    Update BannerHome.jsx

--- frontend/src/components/BannerHome.jsx
+++ frontend/src/components/BannerHome.jsx
@@ -1,6 +1,6 @@
 
 <>
-
+₹
   {/* Decorative circles */}
   <div className="hidden sm:block absolute top-6 left-6 w-20 h-20 rounded-full bg-teal-100 opacity-30"></div>
   <div className="hidden md:block absolute bottom-12 right-28 w-32 h-32 rounded-full bg-seafoam-200 opacity-30"></div>
